073b82b9b7f8c3a7a4324b78878b553a1b2bdf15
vchittooryborn <92055763+vchittooryborn@users.noreply.github.com>
2021-12-22 09:25:26 -0500

    RC incorporated

--- sfcc_cartridge/int_fast_sfra/cartridge/scripts/utils/cleanupUtils.js
+++ sfcc_cartridge/int_fast_sfra/cartridge/scripts/utils/cleanupUtils.js
@@ -18,7 +18,7 @@ function cleanupLogs(params) {
 	//Read the input data from Jobs
 	var sourceFolder : String = params.get('sourceFolder');
 	var noofDays  = params.get('numberofDays');
-   //Check the mandatory inputs
+        //Check the mandatory inputs
 	if(!empty(sourceFolder) && !empty(noofDays))
 	{
 		//Add Impex path in input folders
@@ -31,19 +31,19 @@ function cleanupLogs(params) {
 			currDate.setDate(currDate.getDate() - noofDays);
 			Logger.info('Files before  ' + currDate + 'will be cleaned up'); 
 			var files = sourceDir.listFiles();
-            for each (var file in files){
+                        for each (var file in files){
 				if ((file.exists()) || (file.isFile())) {
 					var fName = file.name;
-                    var fnameindx = fName.indexOf("-");
-                    fName = fName.substring(fnameindx+1);
-                    fName = fName.substr(0,8);
+                                        var fnameindx = fName.indexOf("-");
+                                        fName = fName.substring(fnameindx+1);
+                                        fName = fName.substr(0,8);
 					let fileDate =new Date(fName.substr(0,4),fName.substr(4,2)-1,fName.substr(6,2));
 					if (currDate.getTime() > fileDate.getTime()){
 						Logger.info('File exists in path and the path is ' + file.fullPath);
 						FileUtils.deleteDirectory(file);
-      				}				
+      				        }				
 				}
-            }
+                 }
         }
 	}else{
 		var error = 'Error : one or more mandatory parameters are missing from Jobs Input'
@@ -64,18 +64,17 @@ function cleanupLogs(params) {
 	Logger.info('Entering deleteDirectory () ');
   	var files = sourceDir.listFiles();
           for each(var xfile in files){
-				if ((xfile.exists()) || (xfile.isFile())) {
-					xfile.remove();
-				}
+		if ((xfile.exists()) || (xfile.isFile())) {
+			xfile.remove();
 			}
-    sourceDir.remove();
+		}
+        sourceDir.remove();
 	Logger.info('Exited deleteDirectory () ');
-};
+  };
 
 /* Exported functions*/
 module.exports = {
 	cleanupLogs:cleanupLogs
-	
 };
 /* Exported functions */
 if (typeof (exports) !== 'undefined') {
